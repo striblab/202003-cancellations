d312525278dfbaf016dc6e259b88a637018c3e6b
Thomas Oide <thomas.t.oide@gmail.com>
2020-03-16 11:04:01 -0500

    working data script

--- data/sheet_to_s3.py
+++ data/sheet_to_s3.py
@@ -5,53 +5,85 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
+# need to create a .env file with these variables. ask thomas for key
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID', '')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', '')
 AWS_S3_BUCKET = os.environ.get('AWS_S3_BUCKET', '')
 SHEET_ID = os.environ.get('SHEET_ID', '')
 KEYFILE = os.environ.get('JSON_KEY', '')
 
-def sheet_to_json(obj, filename):
-    bday_json = []
-    for row in islice(obj, 1, None):
-        name = row[0]
-        greeting = row[2]
-        publish = row[3]
+# switch function to parse spaces out of category string
+def switch(i):
+    switcher = {
+        "Arts":'arts',
+        "Business": 'business',
+        "Concert": "concert",
+        "Free service": "free",
+        "Government": "gov",
+        "Religious": "religious",
+        "School (K-12)": "k12_school",
+        "School (University/college)": "college",
+        "Sports": "sports",
+        "Other event": "other"
+    }
+    return switcher.get(i, "")
 
-        if not row or not greeting or not publish:
+# function to turn gsheet data to dictionary. row positions are hardcoded which is less than ideal
+def sheet_to_json(obj, filename):
+    cancel_json = []
+    for row in islice(obj, 2, None):
+        timestamp = row[0]
+        name = row[1]
+        category = switch(row[2])
+        end_date = row[3]
+        city = row[4]
+        description = row[5]
+        cancel_date = row[6]
+        venue = row[7]
+        planned_time = row[8]
+        planned_end_time = row[9]
+
+        if not row:
             continue
         else:
             obj_props = {
-                "name": name,
-                "greeting": greeting,
-                "publish": publish
+                "timestamp": timestamp,
+                "event name": name,
+                "category": category,
+                "city": city,
+                "cancel_date": cancel_date,
+                "planned_time": planned_time,
+                "end_date": end_date,
+                "planned_end_time": planned_end_time,
+                "venue": venue,
+                "desc": description
             }
-
-            bday_json.append(obj_props)
+            cancel_json.append(obj_props)
 
     with open(filename, 'w') as f:
-        json.dump(bday_json, f)
+        json.dump(cancel_json, f)
 
     return
 
+# hook up gspread credentials
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 credentials = ServiceAccountCredentials.from_json_keyfile_name(KEYFILE, scope)
 gc = gspread.authorize(credentials)
 
+# open sheet
 sheet = gc.open_by_key('1bVb5PvL8uWxOGD60luDFOLalWICwKkF9ycko5imsCjE').get_worksheet(0)
 sheet_array = sheet.get_all_values()
 
-print(sheet_array)
+sheet_to_json(sheet_array, './cancellations.json')
 
-# sheet_to_json(sheet_array, './birthday.json')
+# push json to static
+s3 = boto3.client(
+    's3',
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+)
 
-# s3 = boto3.client(
-#     's3',
-#     aws_access_key_id=AWS_ACCESS_KEY_ID,
-#     aws_secret_access_key=AWS_SECRET_ACCESS_KEY
-# )
-#
-# s3_path = 'news/projects/all/20200315-sid-birthday/sid-greetings/'
-# output = s3_path + 'birthday.json'
-#
-# s3.upload_file('./birthday.json', AWS_S3_BUCKET, output)
+s3_path = 'news/projects/all/202003-cancellations/'
+output = s3_path + 'cancellations.json'
+
+s3.upload_file('./cancellations.json', AWS_S3_BUCKET, output)
